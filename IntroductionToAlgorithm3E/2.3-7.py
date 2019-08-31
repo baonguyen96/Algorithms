0d23c60d26033d03bc9a840643b816f9f928e960
baonguyen96 <baonguyendrump@gmail.com>
2019-08-31 12:40:57 -0500

    Initial commit

---
+++ IntroductionToAlgorithm3E/2.3-7.py
@@ -0,0 +1,14 @@
+"""
+Problem 2.3-7
+Describe a O(nlogn)-time algorithm that,
+    given a set S of n integers and another integer x,
+    determines whether or not there exist two elements in S
+    whose sum is exactly x.
+"""
+
+
+def exist_elements_with_sum(s, x):
+    pass
+
+
+

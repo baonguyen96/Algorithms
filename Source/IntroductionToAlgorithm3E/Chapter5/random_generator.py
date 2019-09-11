2d4e50730c65d85936db8b9d421de8d78ac7ce26
baonguyen96 <baonguyendrump@gmail.com>
2019-09-10 23:06:08 -0500

    Add random_generator algorithm and unit tests

---
+++ Source/IntroductionToAlgorithm3E/Chapter5/random_generator.py
@@ -0,0 +1,38 @@
+"""
+Problem 5.1-2
+
+Describe an implementation of the procedure
+    RANDOM(a,b) that only makes calls to RANDOM(0,1).
+    What is the expected running time of your procedure,
+    as a function of aa and bb?
+"""
+
+import random
+
+
+def get_random_number_brute_force(a, b):
+    # O(b - a) - this is not really true because it assume
+    # each time random.randint(0, 1) always return 1
+    # this actually always returns a
+    rand = random.randint(0, 1)
+
+    while rand < a or rand > b:
+        rand += random.randint(0, 1)
+
+    return rand
+
+
+def get_random_number_advance(a, b):
+    # O(log(b - a))
+    if a == b:
+        return a
+
+    rand = random.randint(0, 1)
+    mid = (a + b) // 2
+
+    if rand == 0:
+        rand = get_random_number_advance(a, mid)
+    else:
+        rand = get_random_number_advance(mid, b)
+
+    return rand
---
+++ Test/IntroductionToAlgorith3E/Chapter5/random_generator_test.py
@@ -0,0 +1,27 @@
+from IntroductionToAlgorithm3E.Chapter5.random_generator import get_random_number_brute_force, get_random_number_advance
+from Test.unit_test_template import UnitTestTemplate
+
+
+class MyTestCase(UnitTestTemplate):
+    def test_get_random_number_same(self):
+        x = get_random_number_brute_force(1, 1)
+        self.assertEqual(1, x)
+
+        x = get_random_number_advance(1, 1)
+        self.assertEqual(1, x)
+
+    def test_get_random_number_consecutive(self):
+        x = get_random_number_brute_force(1, 2)
+        self.assertTrue(1 <= x <= 2)
+
+        x = get_random_number_advance(1, 2)
+        self.assertTrue(1 <= x <= 2)
+
+    def test_get_random_number_brute_force_huge_range(self):
+        x = get_random_number_brute_force(10000000, 1000000000)
+        self.assertTrue(10000000 <= x <= 1000000000)
+        self.assertEqual(10000000, x)
+
+    def test_get_random_number_advance_huge_range(self):
+        x = get_random_number_advance(10000000, 1000000000)
+        self.assertTrue(10000000 <= x <= 1000000000)

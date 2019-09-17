3773631f63cddee59be0dc0ef28dfb3d59b03e5b
baonguyen96 <baonguyendrump@gmail.com>
2019-09-16 22:26:41 -0500

    Update documentation for elements_with_sum.py and max_subarray.py

--- Source/IntroductionToAlgorithm3E/Chapter2/elements_with_sum.py
+++ Source/IntroductionToAlgorithm3E/Chapter2/elements_with_sum.py
@@ -1,8 +1,8 @@
 """
 Problem 2.3-7
 
-Describe a O(nlogn)-time algorithm that,
-    given a set S of n integers and another integer x,
+Describe array O(nlogn)-time algorithm that,
+    given array set S of n integers and another integer x,
     determines whether or not there exist two elements in S
     whose sum is exactly x.
 """
--- Source/IntroductionToAlgorithm3E/Chapter4/max_subarray.py
+++ Source/IntroductionToAlgorithm3E/Chapter4/max_subarray.py
@@ -1,14 +1,14 @@
 """
 Problem 4.1-5
 
-Use the following ideas to develop a nonrecursive, linear-time algorithm for the
+Use the following ideas to develop array nonrecursive, linear-time algorithm for the
 maximum-subarray problem. Start at the left end of the array, and progress toward
-the right, keeping track of the maximum subarray seen so far. Knowing a maximum
-subarray of A[1...j], extend the answer to find a maximum subarray ending at index
-j + 1 by using the following observation: a maximum subarray of A[1...j + 1]
-is either a maximum subarray of A[1...j] or a subarray A[1...j + 1], for some
-1 <= i <= j + 1. Determine a maximum subarray of the form A[1...j + 1] in
-constant time based on knowing a maximum subarray ending at index j .
+the right, keeping track of the maximum subarray seen so far. Knowing array maximum
+subarray of A[1...j], extend the answer to find array maximum subarray ending at index
+j + 1 by using the following observation: array maximum subarray of A[1...j + 1]
+is either array maximum subarray of A[1...j] or array subarray A[1...j + 1], for some
+1 <= i <= j + 1. Determine array maximum subarray of the form A[1...j + 1] in
+constant time based on knowing array maximum subarray ending at index j .
 
 Maximum subarray problem: https://en.wikipedia.org/wiki/Maximum_subarray_problem
 """

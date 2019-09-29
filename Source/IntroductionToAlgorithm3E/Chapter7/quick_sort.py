f1b0b2cd9c40e20d0e18b8ec6574d838ad420926
baonguyen96 <baonguyendrump@gmail.com>
2019-09-29 14:05:25 -0500

    Update quick_sort to include quick sort with random partition

--- Source/IntroductionToAlgorithm3E/Chapter7/quick_sort.py
+++ Source/IntroductionToAlgorithm3E/Chapter7/quick_sort.py
@@ -1,4 +1,4 @@
-from IntroductionToAlgorithm3E.Chapter7.partition import partition
+from IntroductionToAlgorithm3E.Chapter7.partition import partition, random_partition
 
 
 def quick_sort(array):
@@ -10,3 +10,14 @@ def _quick_sort(array, low, high):
         pv = partition(array, low, high)
         _quick_sort(array, low, pv - 1)
         _quick_sort(array, pv + 1, high)
+
+
+def quick_sort_random(array):
+    _quick_sort_random(array, 0, len(array) - 1)
+
+
+def _quick_sort_random(array, low, high):
+    if low < high:
+        pv = random_partition(array, low, high)
+        _quick_sort_random(array, low, pv - 1)
+        _quick_sort_random(array, pv + 1, high)
--- Test/IntroductionToAlgorithm3E/Chapter7/quick_sort_test.py
+++ Test/IntroductionToAlgorithm3E/Chapter7/quick_sort_test.py
@@ -1,21 +1,41 @@
 from random import shuffle
 
-from IntroductionToAlgorithm3E.Chapter7.quick_sort import quick_sort
+import IntroductionToAlgorithm3E.Chapter7.quick_sort as qs
 from Test.unit_test_template import UnitTestTemplate
 
 
 class QuickSortTest(UnitTestTemplate):
-    def test_quick_sort(self):
+    def test_quick_sort_small(self):
         array = [1, 2, 3, 4, 5]
-        quick_sort(array)
+        qs.quick_sort(array)
         self.assertEqual([1, 2, 3, 4, 5], array)
 
         shuffle(array)
-        quick_sort(array)
+        qs.quick_sort(array)
         self.assertEqual([1, 2, 3, 4, 5], array)
 
-    def test_quick_sort_random(self):
-        array = list(range(1000))
+    def test_quick_sort_random_small(self):
+        array = [1, 2, 3, 4, 5]
+        qs.quick_sort_random(array)
+        self.assertEqual([1, 2, 3, 4, 5], array)
+
         shuffle(array)
-        quick_sort(array)
-        self.assertEqual(list(range(1000)), array)
+        qs.quick_sort_random(array)
+        self.assertEqual([1, 2, 3, 4, 5], array)
+
+    def test_quick_sort_big_sorted(self):
+        array = list(range(10000))
+        with self.assertRaises(RecursionError):
+            qs.quick_sort(array)
+
+    def test_quick_sort_random_big_sorted(self):
+        array = list(range(10000))
+        qs.quick_sort_random(array)
+        self.assertEqual(list(range(10000)), array)
+
+    def test_quick_sort_random_duplicated(self):
+        array = list(range(1, 10)) * 2
+        qs.quick_sort_random(array)
+        expected_array = sorted(list(range(1, 10)) * 2)
+        self.assertEqual(expected_array, array)
+
